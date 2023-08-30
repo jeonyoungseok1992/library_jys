40402ddeace46eeb440dd223efb17c9216bd2987
mirac <mirac@DESKTOP-HA0B8BO>
2023-08-30 23:21:50 +0800

    first library

---
+++ java/src/com/kh/library/Book.java
@@ -0,0 +1,36 @@
+package com.kh.library;
+
+//Book객체를 만들기위한 클레스
+public class Book {
+	private String title;
+	private String author;
+	private int code;
+	private boolean isRent;
+	
+	public Book(String title, String author, int code) {
+		this.title = title;
+		this.author = author;
+		this.code = code;
+		this.isRent = true;
+	}
+	
+	public void setIsRent(boolean isRent) {
+		this.isRent = isRent;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+//	public void setCode(int code) {
+//		this.code = code;
+//	}
+	public boolean getIsRent() {
+		return this.isRent;
+	}
+	
+	@Override
+	public String toString() {
+		return this.code + "\t" + this.title + "\t" + this.author + "\t" + (this.isRent ? "대여가능": "대여불가");
+	}
+}
---
+++ java/src/com/kh/library/Run.java
@@ -0,0 +1,10 @@
+package com.kh.library;
+
+import java.util.Scanner;
+public class Run {
+	public static void main(String[] args) {
+		Library library = new Library();
+		library.mainMenu();
+		// library객체 안에 createBook 매서드를 작성해보세요.
+	}
+}
